d4388ea8ffbc76cb9b6c09a975ff0249ca465687
Jamadrac <rabbikindalo001@gmail.com>
2023-02-26 12:48:48 +0200

    worked

--- multivender/core/views.py
+++ multivender/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from store.models import Product
+
+def frontpage(request):
+      #return httpresponse(dfghjklkjhgfd)
+    products = Product.objects.filter(status=Product.ACTIVE)[0:6]
