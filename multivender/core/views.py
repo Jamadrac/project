6a8540e92a11151091982315ebfbec5b25fc82b1
Jamadrac <rabbikindalo001@gmail.com>
2023-02-26 12:50:40 +0200

    .

--- multivender/core/views.py
+++ multivender/core/views.py
@@ -8,3 +8,11 @@ from store.models import Product
 def frontpage(request):
       #return httpresponse(dfghjklkjhgfd)
     products = Product.objects.filter(status=Product.ACTIVE)[0:6]
+
+    return render(request, 'core/frontpage.html', {
+        'products': products
+    })
+
+def about(request):
+    #return httpresponse(dfghjklkjhgfd)
+    return render(request, 'core/about.html')
